f6abda7e155e0c22c85200680b7a2af89c79a190
Baptiste <baptiste-lopez@hotmail.com>
2024-07-20 20:04:59 +0200

    Fix Merge

--- scripts/Fan/Fan.gd
+++ scripts/Fan/Fan.gd
@@ -3,6 +3,10 @@ class_name Fan
 extends Node2D
 
 @export var Activated: bool = false
+@export var Wind2DSprite: Sprite2D
+
+var isActivated: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,11 +14,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_key_pressed(KEY_A)):
-		#print("try to activate")
-		Activated = true
+		if (isActivated == false):
+			_fanActivation()
+			Activated = true
+			isActivated = true
 	else:
-		Activated = false
+		if (isActivated == true):
+			_fanActivation()		
+			Activated = false
+			isActivated = false
 	pass
+
+func _fanActivation():
+	Wind2DSprite.visible = not Wind2DSprite.visible
 
 func _get_status():
 	return Activated
