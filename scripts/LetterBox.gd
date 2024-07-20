931c19d3f744b4e118a15bef1a0a9c6acc584836
Adam <adam.robert.motta@gmail.com>
2024-07-21 00:40:12 +0200

    letterbox

---
+++ scripts/LetterBox.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var animation = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func closing():
+	animation.play("closing_door")
+	
+func isClosed():
+	print("End Level !!")
+	#SceneSwitcher.switch_scene("")
