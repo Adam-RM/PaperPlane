5d4fe5c40bb8ce624216d6f5654db9385c24b35a
Baptiste <baptiste-lopez@hotmail.com>
2024-07-20 19:33:23 +0200

    Add Fan sprite and wind method

--- scripts/Fan/Fan.gd
+++ scripts/Fan/Fan.gd
@@ -3,18 +3,29 @@ class_name Fan
 extends Node2D
 
 @export var Activated: bool = false
+@export var Wind2DSprite: Sprite2D
+
+var isActivated: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Calledevery frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_key_pressed(KEY_A)):
-		#print("try to activate")
-		Activated = true
+		if (!isActivated):
+			_hide_show_wind()
+			Activated = true
+			isActivated = true
 	else:
-		Activated = false
+		if(isActivated == true):
+			_hide_show_wind()
+			Activated = false
+			isActivated = false
 	pass
+
+func _hide_show_wind():
+	Wind2DSprite.visible = not Wind2DSprite.visible
 
 func _get_status():
 	return Activated
