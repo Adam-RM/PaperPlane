71e95eb52b035c87608287e087ae69b792ca90d4
Tristan Bouillot <tristanblt@gmail.com>
2024-07-21 11:10:51 +0200

    Update window size and stretch mode for better display

--- scripts/Fan/Fan.gd
+++ scripts/Fan/Fan.gd
@@ -2,35 +2,29 @@ class_name Fan
 
 extends Node2D
 
-@export var FanActivateStatus: bool = false
-@export var Wind2DSprite: AnimatedSprite2D
-@export var Fan2DSprite: AnimatedSprite2D
-
-
+var fanSprite = null
+var windSprite = null
+var status: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("past")
-	Fan2DSprite.pause()
+	fanSprite = get_node("Fan2DSprite") as AnimatedSprite2D
+	windSprite = get_node("Wind2DSprite") as AnimatedSprite2D
+	
+	fanSprite.pause()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	pass
 
 func _fanActivation():
-	Wind2DSprite.visible = not Wind2DSprite.visible
-	if (Fan2DSprite.is_playing()):
-		Fan2DSprite.pause()
+	status = !status
+	windSprite.visible = not windSprite.visible
+	if (fanSprite.is_playing()):
+		fanSprite.pause()
 	else:
-		Fan2DSprite.play()
+		fanSprite.play()
 
 func _get_status():
-	return FanActivateStatus
-
-
-func _on_key_cap_fan_key_pressed_event():
-	_fanActivation()
-	FanActivateStatus = !FanActivateStatus
-	pass # Replace with function body.
+	return status
